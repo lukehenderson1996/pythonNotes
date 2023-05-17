25795c2b090b054c7e0c24027feb8e1d10127198
lukehenderson1996 <lukehenderson1996@yahoo.com>
2023-05-17 14:58:55 -0500

    gui fonts testing

--- scripts/test_gui.py
+++ scripts/test_gui.py
@@ -29,6 +29,26 @@ dw.start()
 # time.sleep(.1)
 # win32gui.SetForegroundWindow(hwnd)
 
+#test fonts:
+import tkinter.font as tkf
+fontTuple = tkf.families()
+dt.info(fontTuple, 'fontTuple')
+dt.genPyLiteral(fontTuple, 'fontTuple')
+#display fonts:
+labelList = []
+for i, font in enumerate(fontTuple):
+    labelList.append(gui.LABEL(color='Black', size=18, font=font))
+    labelList[-1].set("fgijlpqtuvxy 00:1234  " + font, x=20, y=100+20+30*i)
+    guiQ.put(labelList[-1])
+
+customFontLabel = gui.LABEL(color='Black', size=18, font='Nimbus Roman')
+customFontLabel.set("fgijlpqtuvxy 00:1234  " + 'Nimbus Roman', x=20, y=100+20+30*i)
+guiQ.put(customFontLabel)
+
+ut.pause()
+
+
+
 # #simple tests
 # guiQ.put('Message from beyond')
 # guiQ.put(1)
@@ -89,43 +109,43 @@ dw.start()
 # ut.pause()
 
 
-#grid output
-xRange = 2500 #oversize
-yRange = 1500 #oversize
-labelRes = 40 #labels will overlap if too small
-fontSize = 8
-for yPos in range(50, yRange, 200):
-    xGrid = []
-    for xLoc in range(0, xRange, labelRes):
-        xGrid.append(gui.LABEL(size=fontSize, color='green'))
-        xGrid[-1].set(xLoc, x=xLoc, y=yPos)
-        guiQ.put(xGrid[-1])
-    time.sleep(0.1)
-for xPos in range(50, xRange, 200):
-    yGrid = []
-    for yLoc in range(0, yRange, labelRes):
-        yGrid.append(gui.LABEL(size=fontSize, color='purple'))
-        yGrid[-1].set(yLoc, x=xPos, y=yLoc)
-        guiQ.put(yGrid[-1])
-    time.sleep(0.1)
+# #grid output
+# xRange = 2500 #oversize
+# yRange = 1500 #oversize
+# labelRes = 40 #labels will overlap if too small
+# fontSize = 8
+# for yPos in range(50, yRange, 200):
+#     xGrid = []
+#     for xLoc in range(0, xRange, labelRes):
+#         xGrid.append(gui.LABEL(size=fontSize, color='green'))
+#         xGrid[-1].set(xLoc, x=xLoc, y=yPos)
+#         guiQ.put(xGrid[-1])
+#     time.sleep(0.1)
+# for xPos in range(50, xRange, 200):
+#     yGrid = []
+#     for yLoc in range(0, yRange, labelRes):
+#         yGrid.append(gui.LABEL(size=fontSize, color='purple'))
+#         yGrid[-1].set(yLoc, x=xPos, y=yLoc)
+#         guiQ.put(yGrid[-1])
+#     time.sleep(0.1)
 
-#various labels
-myLabel = gui.LABEL(color='Blue', size=12, font="Helvetica")
-myLabel.set("Size 12 @ 300x300", x=300, y=300)
-guiQ.put(myLabel)
-myLabel2 = gui.LABEL(color='Blue', size=18, font="Helvetica")
-myLabel2.set("Size 18 @ 400x400", x=400, y=400)
-guiQ.put(myLabel2)
-myLabel3 = gui.LABEL(color='Blue', size=26, font="Helvetica")
-myLabel3.set("Size 26 @ 600x600", x=600, y=600)
-guiQ.put(myLabel3)
+# #various labels
+# myLabel = gui.LABEL(color='Blue', size=12, font="Helvetica")
+# myLabel.set("Size 12 @ 300x300", x=300, y=300)
+# guiQ.put(myLabel)
+# myLabel2 = gui.LABEL(color='Blue', size=18, font="Helvetica")
+# myLabel2.set("Size 18 @ 400x400", x=400, y=400)
+# guiQ.put(myLabel2)
+# myLabel3 = gui.LABEL(color='Blue', size=26, font="Helvetica")
+# myLabel3.set("Size 26 @ 600x600", x=600, y=600)
+# guiQ.put(myLabel3)
 
-#fill up rolling printer
-for i in range(44):
-    prStr = str((str(i)+' ')*200)[:90]
-    if i%2:
-        prStr = '012345678 10---15---20---25---30---35---40---45---50---55---60---65---70---75---80---85---'
-    guiQ.put(prStr)
+# #fill up rolling printer
+# for i in range(44):
+#     prStr = str((str(i)+' ')*200)[:90]
+#     if i%2:
+#         prStr = '012345678 10---15---20---25---30---35---40---45---50---55---60---65---70---75---80---85---'
+#     guiQ.put(prStr)
 
-time.sleep(2)
-ut.pause()
+# time.sleep(2)
+# ut.pause()
