5634e88ad83f80e798fb380001b1e91c20346f42
lukehenderson1996 <lukehenderson1996@yahoo.com>
2023-05-16 23:17:20 -0500

    gui.py bugfix #2

--- scripts/gui.py
+++ scripts/gui.py
@@ -151,7 +151,7 @@ class GUI:
             iconImg = tk.PhotoImage(file='/home/luke/Documents/projects/niceHash/pics/my icon.gif')
         else:
             baseDir = os.path.dirname(os.getcwd())
-            baseDir += f'\\pics\\my icon.gif'
+            iconImg = tk.PhotoImage(file=baseDir+'\\pics\\my icon.gif')
         self.root.tk.call('wm', 'iconphoto', self.root._w, iconImg)
         # self.root.iconbitmap('@/home/luke/Documents/projects/niceHash/icons/my 64 icon6.xbm')
         # self.root.iconphoto(True, '/home/luke/Documents/projects/niceHash/icons/my 64 icon4.png')
